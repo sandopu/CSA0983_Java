8a26196771c58fde63e11600dbdce05028993560
Rakesh <112846724+sandopu@users.noreply.github.com>
2023-07-05 22:14:50 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int reverse = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+
+        System.out.println("Reverse Number: " + reverse);
+    }
+}
